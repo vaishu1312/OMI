7a211f44e22769fb42449929dfe7ad48bb4d1554
Vaishali R <vaishali17181@cse.ssn.edu.in>
2020-06-10 20:34:43 +0530

    Expirement with widgets and layout Created a commit button

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
 void main(){ 
-  runApp(MyApp());
+  runApp(MyApp()); //MyApp is a user created widget that is built using flutter native widget MaterialApp
 }
 
 class MyApp extends StatelessWidget {
@@ -11,30 +11,107 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Welcome to Flutter',
       theme: ThemeData(        
-        primarySwatch: Colors.red,      //color of title    
+        primarySwatch: Colors.blue,      //color of title    
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),  //visible
-        ),
-        body: Center(
-          child : RandomWords(),
-        ),
-      ),
+      //MaterialApp has a home property to specify the user interface of the home page,which is again a user created widget, RandomWords.
+      home: RandomWords(title:'Demo1'),       
     );
   }
 }
 
 class RandomWords extends StatefulWidget {
+  final  String title;
+  RandomWords({Key key, this.title}) : super(key: key);  
   @override
-  RandomWordsState createState() => RandomWordsState();
+  _RandomWordsState createState() => _RandomWordsState();
 }
 
-class RandomWordsState extends State<RandomWords> {
+class _RandomWordsState extends State<RandomWords> {
   @override
   Widget build(BuildContext context) {
     final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    //RandomWords is build using another flutter native widget, Scaffold
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body:Center(
+        /*child:Text('You have clicked the button this many times',
+                style: Theme.of(context).textTheme.headline4,
+                textAlign: TextAlign.right,
+                textDirection: TextDirection.rtl,
+                overflow: TextOverflow.ellipsis,
+              )
+        child: Text.rich(TextSpan(text:wordPair.asPascalCase,
+                                       style:TextStyle(fontStyle: FontStyle.italic,fontWeight: FontWeight.bold),
+                                ),
+        )
+        child: Text.rich(TextSpan(
+                            children:<TextSpan>[
+                              TextSpan(text:wordPair.asPascalCase,
+                                       style:TextStyle(fontStyle: FontStyle.italic,fontWeight: FontWeight.bold),
+                                      ),
+                              TextSpan(text:wordPair.asPascalCase,
+                                       style:TextStyle(fontStyle: FontStyle.italic,fontWeight: FontWeight.bold),
+                                      ),
+                            ]
+                         ),
+                )*/
+        /*child:Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Text>[
+            Text(
+              'You have clicked the button this many times:',
+               style: Theme.of(context).textTheme.headline4, 
+               textAlign: TextAlign.right,
+               textDirection: TextDirection.ltr,
+               overflow: TextOverflow.ellipsis,
+            ),
+            Text(
+              wordPair.asPascalCase,
+              style: TextStyle(fontWeight: FontWeight.bold),             
+              textAlign: TextAlign.left,
+              textDirection: TextDirection.ltr,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ],
+        ),*/
+        child: Container(
+          //padding: const EdgeInsets.symmetric(horizontal: 20.0,vertical: 2.0),
+          height:100.0,
+          width: 100.0,
+          decoration: const BoxDecoration(
+            border: Border(
+               top: BorderSide(width: 1.0, color: Colors.black),
+               left: BorderSide(width: 1.0, color: Colors.black),
+               right: BorderSide(width: 1.0, color: Colors.black),
+               bottom: BorderSide(width: 1.0, color: Colors.black),
+            ),
+            //color: Colors.grey,
+          ),
+          child: Align(
+              alignment: FractionalOffset(0.5, 0.6),
+              child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0,vertical: 2.0),
+                  decoration: const BoxDecoration(
+                  border: Border(
+                        top: BorderSide(width: 1.0, color: Colors.red),
+                        left: BorderSide(width: 1.0, color: Colors.red),
+                        right: BorderSide(width: 1.0, color: Colors.red),
+                        bottom: BorderSide(width: 1.0, color: Colors.red),
+                    ),
+                    color: Colors.grey,
+                  ),
+                  child: const Text(
+                    'OK',textAlign: TextAlign.center, style: TextStyle(color: Colors.black)
+                  ), 
+              ), 
+          )
+      ),
+      ),
+    );
+    
   }
 }
+
